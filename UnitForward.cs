c6c114ae80a3076bc9988e3c1f7335546f1aebb0
DukeOfRobbins <102866796+DukeOfRobbins@users.noreply.github.com>
2022-04-02 15:50:18 +0100

    Add files via upload

---
+++ UnitForward.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitForward : MonoBehaviour
+{
+    SelectAnObject selectAnObject;
+    public Transform target;
+    Vector3 currentPos;
+    float step = 1.0f;
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
+        {
+        selectAnObject = GetComponent<SelectAnObject>();
+        }
+    private void Update()
+        {
+        moveForward();
+        }
+
+    void moveForward()
+        {
+        currentPos = selectAnObject.currentlySelected.transform.position;
+        if (Input.GetMouseButton(0))
+            {
+            selectAnObject.currentlySelected.transform.position = Vector3.SmoothDamp(currentPos, target.transform.position, ref velocity,1.0f);
+            }
+        }
+    }
